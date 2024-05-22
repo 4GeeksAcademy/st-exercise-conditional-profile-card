5446cf302325bda2b4fce0d8c9ef9d6067c56ef1
StevenSanz <163450220+StevenSanz@users.noreply.github.com>
2024-05-22 18:40:43 +0000

    stFinal

--- src/js/app.js
+++ src/js/app.js
@@ -29,23 +29,38 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  if (variables.name == null) {
+    variables.name = "Your name ";
+  }
+  if (variables.lastName == null) {
+    variables.lastName = "Your lastname";
+  }
+  if (variables.role == null) {
+    variables.role = "Your role";
+  }
+  if (variables.country == null) {
+    variables.country = "Your country";
+  }
+  if (variables.city == null) {
+    variables.city = "Your city";
+  }
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastName}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.country}${variables.city}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="${variables.twitter}" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${variables.github}" target="_blank"><i class="fab fa-github"></i></a></li>
+            <li><a href="${variables.linkedin}" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${variables.instagram}" target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
 }
-
+//<ul class="position-right">
 /**
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
@@ -85,6 +100,7 @@ window.onload = function() {
           : this.value == "false"
           ? false
           : this.value;
+
       render(Object.assign(window.variables, values)); // render again the card with new values
     });
   });
